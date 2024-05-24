8262b6c7294a74afd0e76edf719f0037fcc035bd
guilherme.agostinetti <79881424+GuilhermeCardosoAgostinetti@users.noreply.github.com>
2024-05-24 08:08:45 -0300

    Create declaracao.js

---
+++ Variáveis/declaracao.js
@@ -0,0 +1,36 @@
+
+// TIPOS DE DADOS
+// Numbers: 0,1,2,3, 3.5, 5.8
+// Boolean: true, false
+// String: "Olá mundo"
+
+// Nomenclatura
+// Não Pode começar com números
+// Não pode ter espaço
+// Pode ter Letras, numeros e _
+// Não pode ter palavras reservadas (ex: let)
+
+// Saída de dados
+console.log("Hello World")
+
+
+//Declarando variáveis
+let player;
+let pontos
+let vivo
+
+
+//Atribuindo
+player = "Mario";
+pontos = 0;
+vivo = true;
+
+//Exibindo - Saida de dados
+console.log(player)
+console.log(pontos)
+console.log(vivo)
+
+// Concatenando com Frases
+console.log("Nickname do jogador: ", player)
+console.log("Pontos do jogador: ", pontos)
+console.log("Jogador Vivo: ", vivo)
