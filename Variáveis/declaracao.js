b0a61a8bb053e5f0f0321c14d30d4d462938e4fe
guilherme.agostinetti <79881424+GuilhermeCardosoAgostinetti@users.noreply.github.com>
2024-05-24 08:24:06 -0300

    Update declaracao.js

--- Variáveis/declaracao.js
+++ Variáveis/declaracao.js
@@ -10,14 +10,15 @@
 // Pode ter Letras, numeros e _
 // Não pode ter palavras reservadas (ex: let)
 
+//1)
 // Saída de dados
 console.log("Hello World")
 
-
+//2)
 //Declarando variáveis
 let player;
-let pontos
-let vivo
+let pontos;
+let vivo;
 
 
 //Atribuindo
@@ -26,11 +27,40 @@ pontos = 0;
 vivo = true;
 
 //Exibindo - Saida de dados
-console.log(player)
-console.log(pontos)
-console.log(vivo)
+console.log(player);
+console.log(pontos);
+console.log(vivo);
 
 // Concatenando com Frases
-console.log("Nickname do jogador: ", player)
-console.log("Pontos do jogador: ", pontos)
-console.log("Jogador Vivo: ", vivo)
+console.log("Nickname do jogador: ", player);
+console.log("Pontos do jogador: ", pontos);
+console.log("Jogador Vivo: ", vivo);
+
+
+//3)
+//Declarando variáveis
+let irmao;
+let altura;
+let prova;
+let lampada;
+
+irmao = 3;
+altura = 1.73;
+prova = 'caneta azul';
+lampada = true;
+
+console.log("Ele tem ", irmao, ' irmãos');
+console.log("Ele tem ", altura, ' m de altura');
+console.log("Constava na prova:  ", prova);
+console.log("A lampada está ligada?", lampada);
+
+
+//4)
+//Declarando variáveis de forma simplificada
+let numero = 3;
+let nome = "Guilherme";
+let ligado = true;
+
+
+
+
